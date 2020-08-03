9708658448317f20e97e04bfee5017e4d6f30458
Isaac Glidewell <isaac.j.glidewell@emcins.com>
2020-08-03 14:55:08 -0500

    Final Update to add Standard and Ranked GradeBook classes

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
-    public class StandardGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
         
-        public StandardGradeBook(string name)
-        { }
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
       
     }
 }
