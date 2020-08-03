26ef4e76a620a51b28c62e702bb71789293a922e
Isaac Glidewell <isaac.j.glidewell@emcins.com>
2020-08-03 14:22:59 -0500

    Add StandardGradeBook constructor

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook 
+    {
+        public StandardGradeBook(string name)  
+        {
+
+        }
+    }
+}
