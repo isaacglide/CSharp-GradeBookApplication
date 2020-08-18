a833360cefa1356ac61706cda68491d2d60935af
Isaac Glidewell <isaac.j.glidewell@emcins.com>
2020-08-17 19:42:20 -0500

    Add override to calculateStatistics and calculateStudentStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -33,5 +33,19 @@ namespace GradeBook.GradeBooks
 
             
         }
+
+        public override void CalculateStatistics ()
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else base.CalculateStudentStatistics(name);
+        }
     }
 }
